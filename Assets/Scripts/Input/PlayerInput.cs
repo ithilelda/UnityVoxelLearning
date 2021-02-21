bb633d25cd384ddcca143d74fceac3e71977eeaf
Huawei Wang <huawei_007@yahoo.com>
2021-02-21 22:34:33 +0800

    updated the player input script to have better jumping and movement.

--- Assets/Scripts/Input/PlayerInput.cs
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -7,10 +7,11 @@ public class PlayerInput : MonoBehaviour
 {
     public float MovementSpeed;
     public float RotationSpeed;
-    public float JumpForce;
+    public float JumpHeight;
     public float gravity;
     public CharacterController Controller;
 
+    private Vector3 movement;
     private Vector3 velocity;
     private Vector3 jumpVelocity;
     private Vector3 rotation;
@@ -18,8 +19,8 @@ public class PlayerInput : MonoBehaviour
     public void OnMove(InputValue value)
     {
         var v = value.Get<Vector2>();
-        velocity.x = v.x;
-        velocity.z = v.y;
+        movement.x = v.x;
+        movement.z = v.y;
     }
     public void OnLook(InputValue value)
     {
@@ -31,15 +32,17 @@ public class PlayerInput : MonoBehaviour
     {
         if(Controller.isGrounded)
         {
-            jumpVelocity.y = JumpForce * -gravity;
+            jumpVelocity.y = Mathf.Sqrt(JumpHeight * -gravity * 2f);
         }
     }
 
     private void FixedUpdate()
     {
-        if (Controller.isGrounded && jumpVelocity.y < 0f) jumpVelocity.y = 0f;
+        if (Controller.isGrounded && jumpVelocity.y < 0f) jumpVelocity.y = 0f; // stops falling when we hit ground.
+        if (Controller.isGrounded) velocity = movement; // only move horizontally when we touch ground.
         Controller.Move(transform.TransformDirection(velocity) * MovementSpeed * Time.fixedDeltaTime);
-        jumpVelocity.y += gravity * Time.fixedDeltaTime;
+        var g = jumpVelocity.y >= 0f ? gravity : 2f * gravity;
+        jumpVelocity.y += g * Time.fixedDeltaTime;
         Controller.Move(jumpVelocity * Time.fixedDeltaTime);
 
         var trotate = transform.eulerAngles + rotation * Time.fixedDeltaTime * RotationSpeed;
